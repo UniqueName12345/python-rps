bd604b7541de7ead6807cbf3dd9b5bad264bad7f
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2022-06-02 13:30:19 +0800

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,5 +1,38 @@
-import random, threading
+import random
 
 
-class Computer:
-    
+class Game():
+    def __init__(self):
+        self.choices = ['rock', 'paper', 'scissors']
+        self.player1_score = 0
+        self.player2_score = 0
+        self.player1_name = input('Player 1 name: ')
+        self.player2_name = 'Computer'
+        self.play()
+    def play(self): 
+        while True:
+            choice1 = input(f'{self.player1_name}: ').lower()
+            choice2 = random.choice(self.choices)
+            if choice1 not in self.choices or choice2 not in self.choices:
+                print('Invalid choice')
+                continue
+            print(f'{self.player1_name} chose {choice1}')
+            print(f'{self.player2_name} chose {choice2}')
+            if choice1 == choice2:
+                print('It\'s a tie!')
+            elif (choice1 == 'rock' and choice2 == 'scissors') or (choice1 == 'paper' and choice2 == 'rock') or (choice1 == 'scissors' and choice2 == 'paper'):
+                print(f'{self.player1_name} wins!')
+                self.player1_score += 1
+            else:
+                print(f'{self.player2_name} wins!')
+                self.player2_score += 1
+            print(f'{self.player1_name} score: {self.player1_score}')
+            print(f'{self.player2_name} score: {self.player2_score}')
+            if self.player1_score == 3 or self.player2_score == 3:
+                break
+        print('Game over!')
+        if self.player1_score > self.player2_score:
+            print(f'{self.player1_name} wins!')
+        else: 
+            print(f'{self.player2_name} wins!')
+Game()
